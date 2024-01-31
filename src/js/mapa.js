94fac4f29cf348352a4c7977055df867be05e387
yerson <barrioslafont.5@gmail.com>
2024-01-31 00:51:43 -0500

    se agrega la funcinalidad de detectar el pin y momimiento del pin para autocentrado del mapa

--- src/js/mapa.js
+++ src/js/mapa.js
@@ -1,1 +1,29 @@
-alert('Algo más')
+(function() {
+
+    const lat = 20.67444163271174;
+    const lng = -103.38739216304566;
+    const mapa = L.map('mapa').setView([lat, lng ], 16);
+    let marker;
+
+    
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(mapa);
+
+    //El Pin
+    marker = new L.marker([lat, lng], {
+        draggable: true,
+        autoPan: true
+    })
+    .addTo(mapa)
+
+    //Detectar movimiento del pin
+    marker.on('moveend', function(e){
+        marker = e.target
+
+        const posicion = marker.getLatLng();
+        mapa.panTo(new L.LatLng(posicion.lat, posicion.lng))
+    })
+
+})()
