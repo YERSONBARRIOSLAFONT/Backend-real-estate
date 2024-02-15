56dc4470d8e8618dd59c0e0fbe79173f1836d629
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-02-15 02:19:21 -0500

    adding associations

--- models/index.js
+++ models/index.js
@@ -3,8 +3,9 @@ import Precio from './Precio.js'
 import Categoria from './Categoria.js'
 import Usuario from './Usuario.js'
 
-Precio.hasOne(Propiedad)
-
+Propiedad.belongsTo(Precio, {foreignKey: 'llaveForaneaPrecio'})
+Propiedad.belongsTo(Categoria, {foreignKey: 'categoriaId'})
+Propiedad.belongsTo(Usuario, {foreignKey: 'usuarioId'})
 
 
 export {
