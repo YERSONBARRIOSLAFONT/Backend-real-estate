c8a606bf7de3b8a5ec7a141ab2c3ba215b795a09
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-02-22 21:18:45 -0500

    autofill fields

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,3 +1,4 @@
+import { validationResult } from 'express-validator'
 import Precio from "../models/Precio.js"
 import Categoria from "../models/Categoria.js"
 
@@ -15,16 +16,43 @@ const crear = async (req, res) => {
         Categoria.findAll(),
         Precio.findAll()
     ])
-    
+
     res.render('propiedades/crear', {
         pagina: 'Crear Propiedad',
         barra: true,
+        csrfToken: req.csrfToken(),
         categorias,
-        precios
+        precios,
+        datos: {}
     })
+}
+
+const guardar = async (req, res) => {
+    //Validacion
+    let resultado = validationResult(req)
+
+    if (!resultado.isEmpty()) {
+
+        //Consultar modelo de precio y categorias.
+        const [categorias, precios] = await Promise.all([
+            Categoria.findAll(),
+            Precio.findAll()
+        ])
+
+        return res.render('propiedades/crear', {
+            pagina: 'Crear Propiedad',
+            barra: true,
+            csrfToken: req.csrfToken(),
+            categorias,
+            precios,
+            errores: resultado.array(),
+            datos:req.body
+            })
+    }
 }
 
 export {
     admin,
-    crear
+    crear,
+    guardar
 }
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -1,9 +1,23 @@
 import express from 'express'
-import {admin, crear} from '../controllers/propiedadController.js'
+import { body } from 'express-validator'
+import {admin, crear, guardar} from '../controllers/propiedadController.js'
 
 const router = express.Router()
 
 router.get('/mis-propiedades', admin )
 router.get('/propiedades/crear', crear )
+router.post('/propiedades/crear',
+    body('titulo').notEmpty().withMessage('El titulo de Anuncio es Obligatorio'),
+    body('descripcion')
+        .notEmpty().withMessage('La Descripcion no puede ir vacia')
+        .isLength({max: 500}).withMessage('La Descripcion es muy larga'),
+    body('categoria').isNumeric().withMessage('Selecciona una categoria'),
+    body('precio').isNumeric().withMessage('Selecciona un rango de precios'),
+    body('habitaciones').isNumeric().withMessage('Selecciona la cantidad de habitaciones'),
+    body('estacionamiento').isNumeric().withMessage('Selecciona la cantidad de estacionamientos'),
+    body('wc').isNumeric().withMessage('Selecciona la cantidad de baños'),
+    body('lat').notEmpty().withMessage('Ubica la propiedad en el mapa'),
+    guardar 
+    )
 
 export default router
