c4e2e7142e511becda6e2457b0639936cab5654a
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-02-23 21:04:55 -0500

    finishing autofill of the fields and storing the last location of the pin

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,6 +1,5 @@
 import { validationResult } from 'express-validator'
-import Precio from "../models/Precio.js"
-import Categoria from "../models/Categoria.js"
+import { Precio, Categoria, Propiedad } from '../models/index.js'
 
 const admin = (req, res) => {
     res.render('propiedades/admin', {
@@ -48,6 +47,28 @@ const guardar = async (req, res) => {
             errores: resultado.array(),
             datos:req.body
             })
+    }
+
+    //Crear un registro
+
+    const {titulo, descripcion, habitaciones, estacionamiento, wc, calle, lat, lng, precio: precioId, categoria: categoriaId} = req.body
+
+    try {
+        const propiedadGuardada = await Propiedad.create({
+            titulo,
+            descripcion,
+            habitaciones,
+            estacionamiento,
+            wc,
+            calle,
+            lat,
+            lng,
+            precioId,
+            categoriaId
+
+        })
+    } catch (error) {
+        console.log(error);
     }
 }
 
