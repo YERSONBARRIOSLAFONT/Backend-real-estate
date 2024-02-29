105f824cad9dd14bb7cc2e445bb6cf10afca1437
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-02-29 13:28:47 -0500

    correction of models/index.js I solved the issue in the foreignKey of precio

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ import Precio from './Precio.js'
 import Categoria from './Categoria.js'
 import Usuario from './Usuario.js'
 
-Propiedad.belongsTo(Precio, {foreignKey: 'llaveForaneaPrecio'})
+Propiedad.belongsTo(Precio, {foreignKey: 'precioId'})
 Propiedad.belongsTo(Categoria, {foreignKey: 'categoriaId'})
 Propiedad.belongsTo(Usuario, {foreignKey: 'usuarioId'})
 
