11af5228869a8d56cefc5014704a5a0267922b51
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-03-12 00:14:29 -0500

    shoing the properties in the admin panel

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,9 +1,22 @@
 import { validationResult } from 'express-validator'
 import { Precio, Categoria, Propiedad } from '../models/index.js'
 
-const admin = (req, res) => {
+const admin = async (req, res) => {
+
+    const { id } = req.usuario
+    
+    const propiedades = await Propiedad.findAll({
+        where: {
+            usuarioId : id
+        },
+        include: [
+            {model: Categoria, as: 'categoria'}
+        ]
+    })
+
     res.render('propiedades/admin', {
-        pagina: 'Mis Propiedades'
+        pagina: 'Mis Propiedades',
+        propiedades,
     })
 }
 
