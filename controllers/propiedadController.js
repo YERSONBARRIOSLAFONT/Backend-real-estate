3880f30e9d05c06b909050558621e143d0eeccc3
YERSONBARRIOSLAFONT <barrioslafont.5@gmail.com>
2024-05-30 16:31:37 -0500

    message model and form validation

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -1,7 +1,7 @@
 import { unlink } from 'node:fs/promises'
 import { validationResult } from 'express-validator'
 import { Precio, Categoria, Propiedad } from '../models/index.js'
-import { log } from 'node:console'
+import {esVendedor} from '../helpers/index.js'
 
 const admin = async (req, res) => {
 
@@ -326,11 +326,17 @@ const mostrarPropiedad = async (req, res) => {
         return res.redirect('/404')
     }
 
-    res.render('propiedades/mostrar', {
+        return res.render('propiedades/mostrar', {
         propiedad,
         pagina: propiedad.titulo,
-        csrfToken: req.csrfToken()
+        csrfToken: req.csrfToken(),
+        usuario: req.usuario,
+        esVendedor: esVendedor(req.usuario?.id, propiedad.usuarioId)
     })
+}
+
+const enviarMensaje = async (req, res) => {
+
 }
 
 export {
@@ -342,5 +348,6 @@ export {
     editar,
     guardarCambios,
     eliminar,
-    mostrarPropiedad
+    mostrarPropiedad,
+    enviarMensaje
 }
